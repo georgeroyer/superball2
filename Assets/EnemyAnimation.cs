bd095705d246e9bd30d957c54ab6abc7a2329d38
georgeroyer <georgewroyer3@gmail.com>
2015-05-27 20:52:22 -0500

    Testing Checkin
    testing

---
+++ Assets/EnemyAnimation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimation : MonoBehaviour {
+	public float deadZone = 5f;
+
+	private Transform player;
+	private EnemySight enemySight;
+	private NavMeshAgent nav;
+}
