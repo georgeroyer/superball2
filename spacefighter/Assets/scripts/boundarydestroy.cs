e7f9cf02fa758f4432299e48bd5add8daab90363
georgeroyer <georgewroyer3@gmail.com>
2015-05-31 14:35:55 -0500

    collission working

---
+++ spacefighter/Assets/scripts/boundarydestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boundarydestroy : MonoBehaviour {
+
+	// Use this for initialization
+	void OnTriggerExit(Collider other) {
+		// Destroy everything that leaves the trigger
+		Destroy(other.gameObject);
+	}
+}
+
