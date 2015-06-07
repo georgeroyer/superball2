f3de4ba73e75c2c63de587dfad49ea4674fa0716
georgeroyer <georgewroyer3@gmail.com>
2015-06-07 17:31:39 -0500

    new camera center point rumblr working

---
+++ Assets/scripts/pickupablebackup.cs
@@ -0,0 +1,147 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickupablebackup : MonoBehaviour {
+
+	
+	GameObject grabpoint;
+	GameObject holdpoint;
+	public static bool carrying;
+	public bool held;
+	GameObject carriedObject;
+	public float distance;
+	public float smooth;
+	public float speed;
+	private float throwdirection;
+	public float pullspeed;
+	public GameObject MainCamera;
+	private Vector3 SpawnPosition;
+	private Vector3 dir;
+	
+	// Use this for initialization
+	void Start() {
+		
+		grabpoint = GameObject.FindWithTag("grabpoint"); 
+		holdpoint = GameObject.FindWithTag ("holdpoint");
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (carrying) {
+			carry (carriedObject);
+			
+		} 
+		else 
+		{
+			pickup();
+			
+		}
+		if (held) {
+			holding (carriedObject);
+			
+			checkThrow ();
+		}
+		else
+		{
+			pickup();
+			
+		}
+	}
+	
+	void carry(GameObject o) {
+		dir = holdpoint.transform.position - carriedObject.transform.position;
+		dir = dir.normalized;
+		carriedObject.rigidbody.AddForce (dir * pullspeed);
+		//carriedObject.rigidbody.AddForce (dir * pullspeed);
+		
+		//float step = pullspeed * Time.deltaTime;
+		o.rigidbody.isKinematic = false;
+		//o.transform.position = Vector3.MoveTowards(holdpoint.transform.position, holdpoint.transform.position, step);
+		//o.transform.position = Vector3.Lerp(holdpoint.transform.position, holdpoint.transform.position + holdpoint.transform.forward * distance, Time.deltaTime * smooth);
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		
+		carriedObject.transform.position = Vector3.Lerp (holdpoint.transform.position, holdpoint.transform.position, Time.deltaTime * smooth);
+		Debug.Log ("Object in Trigger");
+		{
+			if
+			(other.gameObject == carriedObject) {
+				//carrying = false;
+				held = true;
+				
+				//carrying = false;
+				//held = true;
+			}
+		}
+	}
+	
+	void holding (GameObject o){
+		carriedObject.transform.position = Vector3.Lerp (holdpoint.transform.position, holdpoint.transform.position, Time.deltaTime * smooth);
+		o.transform.Rotate (new Vector3 (1, 1, 0) * Time.deltaTime);
+	}
+	
+	
+	
+	
+	public void pickup(){
+		
+		if (Input.GetButtonDown("Fire1")) {
+			int x = Screen.width / 2;
+			int y = Screen.height /2;
+			
+			//Ray ray = grabpoint.transform.forward.ScreenPointToRay(new Vector3(x,y));
+			Ray ray = grabpoint.camera.ScreenPointToRay(new Vector3 (x,y));
+			RaycastHit hit;
+			
+			if(Physics.Raycast(ray, out hit)){
+				Pickupable p = hit.collider.GetComponent<Pickupable>();
+				if(p != null){
+					carrying = true;
+					carriedObject = p.gameObject;
+					
+					
+				}
+			}
+		}
+	}
+	
+	void checkThrow (){
+		if (Input.GetButtonDown ("Fire1")) {
+			
+			throwObject ();
+		}
+	}
+	public void throwObject (){
+		carrying = false;
+		held = false;
+		carriedObject.gameObject.rigidbody.isKinematic = false;
+		
+		//throwdirection = (MainCamera.transform.forward);
+		
+		//carriedObject.gameObject.rigidbody.AddTorque (Camera.main.transform.forward * speed);
+		carriedObject.gameObject.rigidbody.AddForce (Camera.main.transform.forward * speed);
+		
+		//carriedObject.gameObject.rigidbody.AddForce 
+		//(throwdirection * speed, 0, 0);
+		
+		
+		
+		//carriedObject.gameObject.rigidbody.transform.TransformDirection (new Vector3 (speed, 0, 0));
+		
+		
+		
+		
+		
+		carriedObject = null;
+		
+		//if (Input.GetKeyDown ("x")) {
+		//SpawnPosition = MainCamera.transform.forward * DistancetoCamera + MainCamera.transform.position;
+		//Instantiate (Cubeprefab, SpawnPosition, Quaternion.identity);
+		
+	}
+}
+
---
+++ Assets/scripts/pickupdetectorbackup.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pickupdetectorbackup : MonoBehaviour {
+
+	
+	
+	public Camera mainCamera;
+	private bool pickupcheck;
+	GameObject rumbleObject;
+	public float speed; 
+	int layerMask = 1 << 9;
+	//private bool carrying;
+	
+	// Use this for initialization
+	void Awake () {
+		mainCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		pickupcheck = GameObject.FindGameObjectWithTag ("Player").GetComponent<PickUpObject>();
+		int layerMask = 1 << 8;
+		//carrying = pickupcheck.carrying(); 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PickUpObject.carrying == false) {
+			//{
+			
+			DetectRay ();
+		}
+		
+	}
+	
+	
+	void DetectRay(){
+		
+		Ray ray = mainCamera.ScreenPointToRay (new Vector3 (Screen.width / 2, Screen.height / 2, 0));	
+		RaycastHit hit;  
+		
+		//if(Physics.Raycast(transform.position, Vector3.forward, Mathf.Infinity, layerMask)){
+		//if (Physics.Raycast (ray, out hit)) {
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, layerMask)) {
+			Pickupable p = hit.collider.GetComponent<Pickupable> ();
+			if (p != null) {
+				print (" pick up at camera center");
+				p.rigidbody.AddForce(0,40,0);
+				//rumbleObject = p.gameObject;
+				//rumbleObject.transform.Rotate (new Vector3 (1, 1, 0) * Time.deltaTime);;
+				//rumbleObject.rigidbody.AddForce (0, 100, 0); 
+			}
+		}
+	}
+	
+	void Shake (){
+		rumbleObject.gameObject.rigidbody.AddForce (0, speed, 0);
+	}
+}
+//void Shake (GameObject rumbleObject){
+//	rumbleObject.rigidbody.AddForce (0, 100, 0);
+//}
+//}
+
